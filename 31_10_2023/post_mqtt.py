3d70f062e961694fd28b325bbad01d08d76d1603
trucxinhhh <93142476+trucxinhhh@users.noreply.github.com>
2023-11-03 16:18:17 +0700

    Update post_mqtt.py

--- 31_10_2023/post_mqtt.py
+++ 31_10_2023/post_mqtt.py
@@ -22,48 +22,41 @@ def on_disconnect(client, userdata, rc):
     
 
 
-def update_master_data(id, led1_status, led2_status,temp, humi, timestamp):
+def update_master_data( led1_status, led2_status,temp, humi):
     data = {
-            "id": id,
             "led1_status": led1_status,
             "led2_status": led2_status,
             "temp": temp,
-            "humi": humi,
-            "timestamp": str(timestamp)
+            "humi": humi
         }   
     json_data = json.dumps(data)
     print(json_data)
     client.publish("update_master_data", json_data)
 
-def update_gateway_data(id, lamp, siren, timestamp):
+def update_gateway_data(lamp, siren):
     data = {
-        "id": id,
         "lamp": lamp,
-        "siren": siren,
-        "timestamp": str(timestamp)
+        "siren": siren
     } 
     json_data = json.dumps(data)
     print(json_data)
     client.publish("update_gateway_data", json_data)
 
 
-def update_node_data(id, vibration, relay, Light_Sensor, Distance_Sensor, timestamp):
+def update_node_data(vibration, relay, Light_Sensor, Distance_Sensor):
     data = {
-        "id": id,
         "vibration": vibration,
         "relay": relay,
         "Light_Sensor": Light_Sensor,
-        "Distance_Sensor": Distance_Sensor,
-        "timestamp": timestamp
+        "Distance_Sensor": Distance_Sensor
     }  
     json_data = json.dumps(data)
     print(json_data)
     client.publish("update_node_data", json_data)
 
 
-def update_all_data(id,led1_status, led2_status,temp, humi, lamp, siren,vibration, relay, Light_Sensor, Distance_Sensor, timestamp):
+def update_all_data(led1_status, led2_status,temp, humi, lamp, siren,vibration, relay, Light_Sensor, Distance_Sensor):
     data = {
-            "id" : id,
             "led1_status": led1_status,
             "led2_status": led2_status,
             "temp": temp,
@@ -73,29 +66,11 @@ def update_all_data(id,led1_status, led2_status,temp, humi, lamp, siren,vibratio
             "vibration": vibration,
             "relay": relay,
             "Light_Sensor": Light_Sensor,
-            "Distance_Sensor": Distance_Sensor,
-            "timestamp": timestamp
+            "Distance_Sensor": Distance_Sensor
     } 
     json_data = json.dumps(data)
     print(json_data)
     client.publish("update_all_data", json_data)
-
-# def create_json(data1, data2, data4):
-#     data = {
-#         "id": int(data1),  # id
-#         "data": int(data2),
-#         "timestamp": str(data4)  
-#     }
-#     json_data = json.dumps(data)
-#     return json_data
-
-
-# def send_single_data_json(data1, data2, data3, data4, data5, data6,data7): 
-#     client.publish("temp_json", create_json(data1,data2,data3,data4))
-#     client.publish("humi_json", create_json(data1,data2,data3,data5))
-#     client.publish("led1_json", create_json(data1,data2,data3,data6))
-#     client.publish("led2_json", create_json(data1,data2,data3,data7))
-#     client.subscribe("post_all_data_json")
 
 
 
@@ -110,7 +85,7 @@ while True:
     humi = float(randint(70,100))
     temp = float(randint(20,30))
     time=str(datetime.now().isoformat())
-    _id=num
+    # _id=num
     led1=randint(0,1)
     led2=randint(0,1)
     lamp=randint(0,1)
@@ -119,10 +94,10 @@ while True:
     relay=randint(0,1)
     Light_Sensor=randint(20,40)
     Distance_Sensor=randint(0,500)
-    update_master_data(_id,led1,led2,temp,humi,time)
-    update_gateway_data(_id,lamp,siren,time)
-    update_node_data(_id,vibration,relay,Light_Sensor,Distance_Sensor,time)
-    update_all_data(_id,led1,led2,temp, humi,lamp,siren,vibration,relay,Light_Sensor,Distance_Sensor,time)
+    update_master_data(led1,led2,temp,humi)
+    update_gateway_data(lamp,siren)
+    update_node_data(vibration,relay,Light_Sensor,Distance_Sensor)
+    update_all_data(led1,led2,temp, humi,lamp,siren,vibration,relay,Light_Sensor,Distance_Sensor)
 
     num+=1
     sleep(3)
